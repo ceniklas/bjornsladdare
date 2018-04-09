913b7b971dbaf5ac39648b57f90b2e671e31e013
Niklas Fransson <ceniklas@gmail.com>
2018-04-09 15:56:40 +0200

    YOLO or twice

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './bjuppa.jpg';
 import './App.css';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        
+        <div className="My-header">
+          <h1 className="App-intro">
+            "Tjong! 
+            Hade "så bråttom" att dra så jag glömde min datorladdare... (En HP)
+            Kan någon vara barmhärtig och dra ur den och märka upp med mitt namn á la den gången Ekroth glömde sin... (kanske inte samma exponering, men...)"
+          </h1>
+        </div>
+
+        <div className="My-Content">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+
+        <div className="My-TwitterContent">
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="bjornsladdare"
+            options={{height: 800}}
+          />
+        </div>
       </div>
     );
   }
